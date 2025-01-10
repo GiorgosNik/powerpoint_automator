a39fccda71d365599fb87bf4be30449312697fb3
Giorgos Nikolaou <giorgosnl17@gmail.com>
2025-01-10 18:17:54 +0200

    Revert "Update main.py"
    This reverts commit f44da766966ae7193fda3a1385213029ca4f05f6.

--- main.py
+++ main.py
@@ -25,8 +25,7 @@ CONFIG = {
     'template_path': "template.pptx",
     'output_pptx': "updated_presentation.pptx",
     'output_video': os.path.join(desktop_path, "Καιρός.mp4"),  # Save video to Desktop
-    'slide_dimensions': {'left': 4.18, 'top': 1.29, 'width': 17.03, 'height': 11.69},
-    'log_path': os.path.join(desktop_path, "app.log")  # Save log to Desktop
+    'slide_dimensions': {'left': 4.18, 'top': 1.29, 'width': 17.03, 'height': 11.69}
 }
 
 # Setup logging with file handler and format
@@ -35,7 +34,7 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[ 
         logging.StreamHandler(),
-        logging.FileHandler(CONFIG['log_path'], mode='w', encoding='utf-8')  # Save log to Desktop
+        logging.FileHandler("app.log", mode='w', encoding='utf-8')
     ]
 )
 
